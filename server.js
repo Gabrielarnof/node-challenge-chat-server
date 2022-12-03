b7a1ab221f39ff8858120e77f2c21604a4f4f948
Gabriel Arno <agafarno@gmail.com>
2022-12-03 12:27:45 +0100

    ds

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const welcomeMessage = {
   id: 0,
   from: "Bart",
@@ -21,5 +23,9 @@ app.get("/", function (request, response) {
 });
 
 app.listen(3000, () => {
-   console.log("Listening on port 3000")
-  });
+  console.log("Listening on port 3000");
+});
+
+app.get("/messages", function (request, response) {
+  response.json(messages);
+});
