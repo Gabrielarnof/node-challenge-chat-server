327747a1d70c096878dfbe9c3b29e513b90964fd
Gabriel Arno <agafarno@gmail.com>
2022-12-03 12:54:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+let availableId = 1;
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
@@ -28,4 +29,17 @@ app.listen(3000, () => {
 
 app.get("/messages", function (request, response) {
   response.json(messages);
+});
+
+app.get("/messages/:id", function (request, response) {
+  let messageId = request.params.id;
+  response.send(JSON.stringify(messages[messageId]));
+});
+
+app.post("/messages", function (request, response) {
+  const { from, text } = request.body;
+  const newMessage = { from, text };
+  newMessage.id = availableId++;
+  messages.push(newMessage);
+  response.sendStatus(201);
 });
